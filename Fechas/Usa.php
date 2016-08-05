0c8494042d79c439b4144b755475b8c63f69201d
Humberto <1201hs@gmail.com>
2016-08-05 12:10:31 -0500

    first information project

---
+++ Fechas/Usa.php
@@ -0,0 +1,69 @@
+<?php
+namespace bathsan\patdate;
+
+/*==============================================================================================================================
+ * @access public
+ * @author Humberto Santos
+ * @version 1.1.x-dev
+==============================================================================================================================*/
+class Usa implements Fecha
+{
+    // --- ATTRIBUTES ---
+    private $_fecha = null;
+
+    /*==============================================================================================================================
+    * Se inicializa las variables
+    ==============================================================================================================================*/
+    public function __construct($fecha)
+    {
+        $this->_fecha = $fecha;
+    }
+
+    /*==============================================================================================================================
+    * Funcion para formato USA
+    ==============================================================================================================================*/
+    // format date mm/dd/YY to ddmmYY
+    public function formato()
+    {
+        $returnValue = null;
+        list($mes, $dia, $ano) = explode("/", $this->_fecha);
+        $returnValue = $dia.$mes.substr($ano, -2);
+        return $returnValue;
+    }
+
+    // Format date ddmmYY to mm/dd/YY
+    public function revDate()
+    {
+        if (strlen($this->_fecha) == 6) {
+            $dia = substr($this->_fecha, 0, 2);
+            $mes = substr($this->_fecha, 2, 2);
+            $ano = substr($this->_fecha, 4, 2);
+        }
+
+        return $mes."/".$dia."/".$ano;
+    }
+
+    // Format date ddmmYY to 
+    public function togethertoDate()
+    {
+        if (strlen($this->_fecha) == 6) {
+            $dia = substr($this->_fecha, 0, 2);
+            $mes = substr($this->_fecha, 2, 2);
+            $ano = substr($this->_fecha, 4, 2);
+        }
+
+        $miFecha= gmmktime(12,0,0, $mes, $dia, $ano);
+
+        return strftime("%A, %B %d %Y", $miFecha);
+    }
+
+    // Format mm/dd/YY to YYmmdd
+    public function formatoMysql()
+    {
+        $returnValue = null;
+        list($mes, $dia, $ano) = explode("/", $this->_fecha);
+        $returnValue = $ano.$mes.$dia;
+        return $returnValue;
+    }
+} 
+?>
